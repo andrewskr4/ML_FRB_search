207ff1162358844919f2d1ad600a2062009897f4
Andrew Sikora <asikora@cedar5.cedar.computecanada.ca>
2021-02-08 08:00:57 -0800

    Changes to which model is used

--- MachineLearning/frb_search/run_model.py
+++ MachineLearning/frb_search/run_model.py
@@ -2,7 +2,8 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import sys
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
 import matplotlib.gridspec as gridspec
 
 from dataclasses import dataclass
@@ -224,8 +225,9 @@ testloader = DataLoader(testset, batch_size=200,shuffle=False, num_workers=2)
 
 model = Net()
 #model.load_state_dict(torch.load('/data/andrew/saved_models/best_model.pth'))
-model.load_state_dict(torch.load('/data/andrew/saved_models/aruns_data_trained_checkpoint_112500.pth', map_location='cuda:0')['model'])
+#model.load_state_dict(torch.load('/data/andrew/saved_models/aruns_data_trained_checkpoint_112500.pth', map_location='cuda:0')['model'])
 #model.load_state_dict(torch.load('/data/andrew/models/first_model.pth',map_location='cuda:0')['model'])
+model.load_state_dict(torch.load('/home/asikora/scratch/July20_model_v2_checkpoint_21400.pth', map_location=torch.device('cpu'))['model'])
 #model.load_state_dict(torch.load('/data/andrew/combined_model.pth',map_location='cuda:0')['model'])
 model.to(device)
 
